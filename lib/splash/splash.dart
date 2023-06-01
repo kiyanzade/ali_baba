9cc534bf2cb751d7c5cbcc57609be4d60914ea30
Kiyanzade <s.Kiyanzade@gmail.com>
2023-06-01 21:31:17 +0330

    Splash screen

---
+++ lib/splash/splash.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
+
+class SplashSceen extends StatelessWidget {
+  const SplashSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade800,
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: Image.asset(
+                    'assets/images/ali_baba_logo.png',
+                  ),
+                ),
+                const SizedBox(
+                  height: 52,
+                ),
+                SpinKitThreeBounce(
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ],
+            ),
+            Positioned(bottom: 10, child: Text('8.9.7'.toPersianDigit()))
+          ],
+        ),
+      ),
+    );
+  }
+}
